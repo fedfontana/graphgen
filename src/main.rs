6d8b0b00e680045a075d36a73b1962886af04bd1
fedfontana <federico.fontana2000@gmail.com>
2023-04-04 17:32:53 +0200

    added out file

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,57 @@
 mod scraper;
-use std::error::Error;
+use std::{error::Error, path};
 
 use clap::Parser;
 
 use crate::scraper::WikipediaScraper;
+
+/// Simple wikipedia scraper
 #[derive(Parser)]
 struct Args {
+
+    /// Url to scrape
     url: String,
+
+    /// Depth of the scrape
     #[clap(short, long, default_value_t = 5, value_parser=clap::value_parser!(u64).range(1..))]
     depth: u64,
+
+    /// Number of threads to use for scraping
     #[clap(short, long, default_value_t = 1, value_parser=clap::value_parser!(u64).range(1..))]
     threads: u64,
+
+    /// Saves the scraped content to a file in csv format
+    #[clap(short, long="output-file")]
+    output_file: Option<String>,
 }
+
+//TODO fix error: sometimes there are commas in the url, escape them so that they don't break the out csv
+//TODO add logging and progress bar
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
 
+    if let Some(output_file_path) = &args.output_file {
+        let out_file_path = path::Path::new(output_file_path);
+        if out_file_path.exists() {
+            return Err("File already exists. Delete it and run the program again if you want to use that path.".into());
+        }
+    }
+
     let mut scraper = WikipediaScraper::new(&args.url, args.depth);
     scraper.scrape()?;
 
-    scraper.links().for_each(|link| println!("{:?}", link));
-    // scraper.pages().for_each(|node| println!("{:?}", node));
+    
+    if let Some(output_file_path) = &args.output_file {
+        scraper.save_to_file(output_file_path)?;
+    } else {
+        //TODO should this be logged?
+        
+        //TODO a single write
+        println!("source,target");
+        scraper.links().for_each(|link| println!("{:?}", link));
+        // scraper.pages().for_each(|node| println!("{:?}", node));
+    }
 
     Ok(())
 }
--- src/scraper.rs
+++ src/scraper.rs
@@ -1,5 +1,5 @@
 use reqwest::blocking::get;
-use std::{collections::HashSet, error::Error, io::Read};
+use std::{collections::HashSet, error::Error, io::{Read, Write}};
 
 pub struct WikipediaScraper<'a> {
     url: &'a str,
@@ -11,13 +11,13 @@ pub struct WikipediaScraper<'a> {
 //TODO filter out images/catergories/other links that don't lead to a page
 
 //TODO some links redirect to the same page, so we need to check for that before adding them to the set
-//TODO maybe build a map of links that redirect to the same page and check against that instead 
+//TODO maybe build a map of links that redirect to the same page and check against that instead
 //TODO of making requests each time
 fn get_complete_url(url: &str) -> Option<String> {
     // All of the internal links start with a slash
     if url.starts_with('/') {
         return Some("https://en.wikipedia.org".to_owned() + url);
-    } 
+    }
     None
 }
 
@@ -37,9 +37,13 @@ impl<'a> WikipediaScraper<'a> {
 
     pub fn scrape(&mut self) -> Result<(), Box<dyn Error>> {
         self.scrape_with_depth(self.url, self.depth)
-    }    
+    }
 
-    fn scrape_with_depth(&mut self, start_url: impl AsRef<str>, depth: u64) -> Result<(), Box<dyn Error>> {
+    fn scrape_with_depth(
+        &mut self,
+        start_url: impl AsRef<str>,
+        depth: u64,
+    ) -> Result<(), Box<dyn Error>> {
         if depth == 0 {
             return Ok(());
         }
@@ -48,24 +52,33 @@ impl<'a> WikipediaScraper<'a> {
         let anchor_list = get_anchor_list(&page_content)?;
 
         for anchor in anchor_list {
-            self.links.insert((start_url.as_ref().to_string().clone(), anchor.clone()));
+            self.links
+                .insert((start_url.as_ref().to_string().clone(), anchor.clone()));
             // if we've already visited this node, skip it
             if !self.pages.contains(&*anchor) {
                 self.pages.insert(anchor.clone());
-                self.scrape_with_depth(anchor, depth-1)?;
+                self.scrape_with_depth(anchor, depth - 1)?;
             }
         }
         Ok(())
     }
 
-    pub fn links(&self) -> impl Iterator<Item=&(String, String)> {
+    pub fn save_to_file(&self, output_file: impl AsRef<str>) -> Result<(), Box<dyn Error>> {
+        let mut file = std::fs::File::create(output_file.as_ref())?;
+        file.write("source,target\n".as_bytes())?;
+        for link in self.links() {
+            file.write_all(format!("{},{}\n", link.0, link.1).as_bytes())?;
+        }
+        Ok(())
+    }
+
+    pub fn links(&self) -> impl Iterator<Item = &(String, String)> {
         self.links.iter()
     }
 
-    pub fn pages(&self) -> impl Iterator<Item=&String> {
+    pub fn pages(&self) -> impl Iterator<Item = &String> {
         self.pages.iter()
     }
-
 }
 
 fn get_page_content(url: impl AsRef<str>) -> Result<String, Box<dyn Error>> {
@@ -75,15 +88,15 @@ fn get_page_content(url: impl AsRef<str>) -> Result<String, Box<dyn Error>> {
     Ok(content)
 }
 
-fn get_anchor_list (page_content: &str) -> Result<Vec<String>, Box<dyn Error>> {
+fn get_anchor_list(page_content: &str) -> Result<Vec<String>, Box<dyn Error>> {
     let document = scraper::Html::parse_document(page_content);
-    
+
     //TODO use errorkind and just log if we can't find the content (right now it is stopping the program)
     let content_selector = scraper::Selector::parse("#bodyContent")?;
-    let content = document.select(&content_selector).next().map_or_else(
-        || Err("No content found"),
-        |content| Ok(content)
-    )?;
+    let content = document
+        .select(&content_selector)
+        .next()
+        .map_or_else(|| Err("No content found"), |content| Ok(content))?;
 
     let anchor_selector = scraper::Selector::parse("a")?;
 
